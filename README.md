004ced4126cecaaf97a1a372618ebabdcdc3b355
venkatram64 <venkat.veerareddy@gmail.com>
2017-09-15 09:46:04 -0400

    init code

---
+++ README.md
@@ -0,0 +1,3 @@
+http://thejavamonkey.blogspot.in/2008/04/clientserver-hello-world-in-kerberos.html
+
+https://gist.github.com/dmansfield/c75817dcacc2393da0a7  (kerberos  node code)
---
+++ src/main/java/com/venkat/jaaskerberos/JaasKerberosApplication.java
@@ -0,0 +1,12 @@
+package com.venkat.jaaskerberos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaasKerberosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaasKerberosApplication.class, args);
+	}
+}
